b352f1b75f1e1e790c6aa2a4ca68fbcc9fb2ee46
Mesbah Uddin <mesbah-uddin@users.noreply.github.com>
2021-05-13 13:36:30 +0900

    chore: call uvicorn from 'main' instead of terminal (now runs with 'cd app && python main.py')

--- fastapihello/app/main.py
+++ fastapihello/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import uvicorn
 
 
 app = FastAPI()
@@ -12,4 +13,8 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
+
+
+if __name__ == '__main__':
+    uvicorn.run(app)
 
